4199e93cf28fade9c5231f106d16ca1c250b84c2
tekac <soehnerj@gmail.com>
2022-11-01 13:44:47 -0400

    had to address the .com protocols on extraction - test 2

--- extract-domain/script.js
+++ extract-domain/script.js
@@ -6,6 +6,10 @@
 
 //6 indexs in http:// .. regex if www remove?
 
-const domainName = (url) => new URL(url).hostname.replace(".com", "");
+const domainName = (url) => {
+  let domain = new URL(url).hostname;
+  let protocol = domain.indexOf(".");
+  return domain.substring(0, protocol);
+};
 
 console.log(domainName("http://github.com/carbonfive/raygun"));
